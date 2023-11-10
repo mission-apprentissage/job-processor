333b502eaec73a12425ac20f83d12b74e04a2449
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-11-10 15:14:20 +0100

    feat: setup project

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import { helloWorld } from './index.ts';
+
+describe('helloWorld', () => {
+  it('should display hello world', () => {
+    expect(helloWorld()).toBe('Hello World')
+  })
+});
