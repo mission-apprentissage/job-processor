2fecbd9ce43834fdd2706d9948365e6070d5e955
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-11-10 15:44:31 +0100

    fix: npm publish

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,6 @@ import { helloWorld } from "./index.ts";
 
 describe("helloWorld", () => {
   it("should display hello world", () => {
-    expect(helloWorld("Moroine")).toBe("Hello Moroine");
+    expect(helloWorld("Antoine")).toBe("Hello Antoine");
   });
 });
