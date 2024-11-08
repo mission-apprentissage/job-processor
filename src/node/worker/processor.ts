707390154cb7c1a9ba2fe3852a9887bbbccb6ac8
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-11-08 13:47:15 +0100

    fix: remove recursive job-processor (#17)

--- src/node/worker/processor.ts
+++ src/node/worker/processor.ts
@@ -7,27 +7,24 @@ import { executeJob, reportJobCrash } from "./worker.ts";
 export const processorEventEmitter = new EventEmitter();
 
 export async function runJobProcessor(signal: AbortSignal): Promise<void> {
-  if (signal.aborted) {
-    getLogger().info("job-processor: abort requested - stopping processor");
-    return;
+  while (!signal.aborted) {
+    const exitedJob = await detectExitedJobs();
+    if (exitedJob) {
+      await reportJobCrash(exitedJob);
+    }
+
+    getLogger().debug(`Process jobs queue - looking for a job to execute`);
+    const nextJob = await pickNextJob();
+
+    if (nextJob) {
+      getLogger().info({ job: nextJob.name }, "job will start");
+      await executeJob(nextJob, signal);
+    } else {
+      await sleep(45_000, signal); // 45 secondes
+    }
+
+    processorEventEmitter.emit("continue");
   }
 
-  const exitedJob = await detectExitedJobs();
-  if (exitedJob) {
-    await reportJobCrash(exitedJob);
-    return runJobProcessor(signal);
-  }
-
-  getLogger().debug(`Process jobs queue - looking for a job to execute`);
-  const nextJob = await pickNextJob();
-
-  if (nextJob) {
-    getLogger().info({ job: nextJob.name }, "job will start");
-    await executeJob(nextJob, signal);
-  } else {
-    await sleep(45_000, signal); // 45 secondes
-  }
-
-  processorEventEmitter.emit("continue");
-  return runJobProcessor(signal);
+  getLogger().info("job-processor: abort requested - stopping processor");
 }
